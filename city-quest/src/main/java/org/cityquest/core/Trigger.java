58c8f6fa3958ddfbe91af53be4c07683722899d0
unknown <unicleonid@yahoo.com>
2013-05-21 03:24:52 +0100

    working on core

---
+++ city-quest/src/main/java/org/cityquest/core/Trigger.java
@@ -0,0 +1,11 @@
+package org.cityquest.core;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: leo
+ * Date: 5/21/13
+ * Time: 3:12 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Trigger {
+}
